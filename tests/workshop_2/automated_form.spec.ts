674af69160492b80ddeed23a5348fd60b788320d
Mivics1 <agbooladaramola7@gmail.com>
2025-02-07 03:30:41 +0100

    PTA-3 Automated form completed

---
+++ tests/workshop_2/automated_form.spec.ts
@@ -0,0 +1,28 @@
+import {test, expect} from "@playwright/test"
+
+test.only('Automated Form', async({page})=>{
+    await page.goto('https://demo.playwright.dev/todomvc/#/');
+    await page.waitForTimeout(3000);
+    
+    //Locating the Textbook and typing the requirement
+    const textbox = await page.getByPlaceholder('What needs to be done?');
+    await textbox.fill('God loves you');
+    await textbox.press('Enter');
+    await textbox.fill('My safe heaven');
+    await textbox.press('Enter');
+    await textbox.fill('You are my safe heaven');
+    await textbox.press('Enter');
+    await page.waitForTimeout(3000);
+
+    //Selecting all Entries
+    const selectAll_entries = await page.locator('#toggle-all');
+    await selectAll_entries.check();
+    // await selectAll_entries.click();
+    await page.waitForTimeout(3000);
+
+    //Delete all Selected entries
+    const delete_button = await page.getByRole('button',{name: 'Clear completed'}).click();
+    await page.waitForTimeout(3000);
+
+
+})
