b031a777726a14bf0d020515e7603392b1420789
Mivics1 <agbooladaramola7@gmail.com>
2025-02-07 02:44:16 +0100

    PTA-2 workshop_1 completed

--- tests/workshop_1/navigating_page.spec.ts
+++ tests/workshop_1/navigating_page.spec.ts
@@ -1,13 +1,11 @@
 import {test} from 'playwright/test'
 
-test('Basic Navigation', async({page})=>{
+test('Basic Navigation',async({page})=>{
     await page.goto('https://gitlab.com/');
+    await page.getByRole('button',{name:'Accept All Cookies'}).click();
+    await page.getByRole('link',{name:'Sign in'}).click();
     await page.waitForTimeout(3000);
-    await page.reload();
+    await page.getByTestId('username-field').fill('Michael');
+    await page.waitForTimeout(3000);
 })
 
-// test.only('Interacting with Web Element on Gitlab',async({page})=>{
-//     await page.goto('https://gitlab.com/');
-//     await page.click('');
-//     await page.locator('');
-// })
