65b1df35ca3a4c6701b7e5a8e9aa146fe227d222
Mivics1 <agbooladaramola7@gmail.com>
2025-02-09 01:31:17 +0100

    PTA-7 Handling different Alerts

---
+++ tests/Workshop_4/handling_alert.spec.ts
@@ -0,0 +1,41 @@
+import {test, expect} from '@playwright/test'
+
+test.skip('Handling Single option Alert',async({page})=>{
+    await page.goto('file:///Users/agbooladaramola/Documents/PlayWright/beginner/tests/workshop_4/index2.html');
+    let alertMessage = '';
+        page.on('dialog',async(dialog)=>{
+            expect(await dialog.type()).toBe('alert');
+            alertMessage = await dialog.message();
+            await dialog.accept();
+        })
+    await page.locator('#show-alert.alert-button').click();
+    expect(await alertMessage).toBe('This is a simple alert.');
+    await page.waitForTimeout(3000);
+})
+
+test.skip('Confirm Alert',async({page})=>{
+    await page.goto('file:///Users/agbooladaramola/Documents/PlayWright/beginner/tests/workshop_4/index2.html');
+    let alertMessage = '';
+        page.on('dialog',async(dialog)=>{
+            alertMessage = await dialog.message();
+            await dialog.dismiss();
+        })
+    await page.locator('#show-confirm.alert-button').click();
+    expect(await alertMessage).toBe('You clicked Cancel.');
+    await page.waitForTimeout(3000);
+})
+
+test.only('Open Pop-up',async({page})=>{
+    await page.goto('file:///Users/agbooladaramola/Documents/PlayWright/beginner/tests/workshop_4/index2.html');
+    const [popup] = await Promise.all([
+        await page.waitForEvent('popup'),
+        await page.click('#open-popup')
+    ]);
+
+    await popup.waitForLoadState();
+    await popup.close();
+    await page.waitForTimeout(3000);
+    // await page.getByText('Open Pop-up').click();
+    // await page.waitForTimeout(3000);
+    // await page.locator('#open-popup.open-popup').click();
+})
